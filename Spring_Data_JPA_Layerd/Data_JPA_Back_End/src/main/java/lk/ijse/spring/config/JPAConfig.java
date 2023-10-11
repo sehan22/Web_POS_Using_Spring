b97cd86c811ab60fd5c6ad67b93874524d31a680
sehan22 <sehanranaweera2002@gmail.com>
2023-10-11 09:36:29 +0530

    Project Finalized

--- Spring_Data_JPA_Layerd/Data_JPA_Back_End/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ Spring_Data_JPA_Layerd/Data_JPA_Back_End/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -4,8 +4,11 @@ import lk.ijse.spring.repo.CustomerRepo;
 import lk.ijse.spring.repo.ItemRepo;
 import lk.ijse.spring.repo.OrderDetailsRepo;
 import lk.ijse.spring.repo.OrdersRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -22,7 +25,11 @@ import javax.sql.DataSource;
 @Configuration
 @EnableTransactionManagement // AOP Usage
 @EnableJpaRepositories(basePackageClasses = {CustomerRepo.class, ItemRepo.class, OrdersRepo.class, OrderDetailsRepo.class})
+@PropertySource("classpath:application.properties")
 public class JPAConfig {
+    @Autowired
+    private Environment environment;
+
     public JPAConfig() {
         System.out.println("JPAConfig : Instantiated..!");
     }
@@ -30,7 +37,8 @@ public class JPAConfig {
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter va) {
         LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
-        factoryBean.setPackagesToScan("lk.ijse.spring.entity");
+//        factoryBean.setPackagesToScan("lk.ijse.spring.entity");
+        factoryBean.setPackagesToScan(environment.getRequiredProperty("pro.entity"));
         factoryBean.setDataSource(ds);
         factoryBean.setJpaVendorAdapter(va);
         return factoryBean;
@@ -39,17 +47,17 @@ public class JPAConfig {
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource ds = new DriverManagerDataSource();
-        ds.setDriverClassName("com.mysql.jdbc.Driver");
-        ds.setUrl("jdbc:mysql://localhost:3306/myworkdb1?createDatabaseIfNotExist=true");
-        ds.setUsername("root");
-        ds.setPassword("Sehan1234@");
+        ds.setDriverClassName(environment.getRequiredProperty("pro.driver"));
+        ds.setUrl(environment.getRequiredProperty("pro.url"));
+        ds.setUsername(environment.getRequiredProperty("pro.username"));
+        ds.setPassword(environment.getRequiredProperty("pro.password"));
         return ds;
     }
 
     @Bean
     public JpaVendorAdapter jpaVendorAdapter() {
         HibernateJpaVendorAdapter va = new HibernateJpaVendorAdapter();
-        va.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+        va.setDatabasePlatform(environment.getRequiredProperty("pro.dial"));
         va.setDatabase(Database.MYSQL);
         va.setGenerateDdl(true);
         va.setShowSql(true);
